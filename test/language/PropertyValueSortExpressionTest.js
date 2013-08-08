eb1d2fa910953255b94928bcfab68b42fc55a0d5
jeroendedauw <jeroendedauw@gmail.com>
2013-08-08 08:03:06 +0200

    Add getPropertyId to PropertyValueSortExpression

--- test/language/PropertyValueSortExpressionTest.js
+++ test/language/PropertyValueSortExpressionTest.js
@@ -4,9 +4,12 @@ var ask = require('../../');
 
 exports.testCanConstructPropertyValueSortExpression = function( test ) {
 	var direction = 'asc'; // TODO: use a constant
-	var sortExpression = new ask.language.PropertyValueSortExpression( direction );
+	var propertyId = 42;
+
+	var sortExpression = new ask.language.PropertyValueSortExpression( propertyId, direction );
 
 	test.equals( sortExpression.getDirection(), direction );
+	test.equals( sortExpression.getPropertyId(), propertyId );
 
 	test.done();
 };
