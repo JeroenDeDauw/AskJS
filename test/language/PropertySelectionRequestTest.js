102706842eb71c44fa1e6a67997efb329770a9b9
jeroendedauw <jeroendedauw@gmail.com>
2013-08-08 06:29:19 +0200

    Add PropertySelectionRequest.getPropertyId

--- test/language/PropertySelectionRequestTest.js
+++ test/language/PropertySelectionRequestTest.js
@@ -3,7 +3,10 @@
 var ask = require('../../');
 
 exports.testCanConstructPropertySelectionRequest = function( test ) {
-	var queryOptions = new ask.Language.PropertySelectionRequest();
+	var propertyId = 42;
+	var propertySelection = new ask.Language.PropertySelectionRequest(propertyId);
+
+	test.equals(propertySelection.getPropertyId(), propertyId);
 
 	test.done();
 };
